fb88604dfddf98bf541c6d43eca9fc0553f0b4a5
bigcraig <craigwoollett@outlook.com>
2018-11-02 16:09:02 +1100

    added a 2nd activity with an intent to start activity as part of onclick handler

--- app/src/main/java/com/example/craig/craigtext/MainActivity.java
+++ app/src/main/java/com/example/craig/craigtext/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.craig.craigtext;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.database.sqlite.SQLiteDatabase;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button btn = findViewById(R.id.Activity_Button);
         final TextView tV = findViewById(R.id.craigView);
         // create database
         db=openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
@@ -42,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btn.setOnClickListener(new View.OnClickListener()
+                               {
+                                   @Override
+                                   public void onClick(View view){
+                                       Intent intent = new Intent(view.getContext(), Main2Activity.class);
+                                       startActivity(intent);
+                                   }
+                               }
+
+        );
     }
 
     @Override
